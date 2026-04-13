fe8fce6ee57ea37ed34777386c1fdd37542006ba
Aq1lla <mkorotaj@gmail.com>
2026-04-13 12:46:22 +0000

    3.- implemented TextType enum and TextNode class

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from textnode import *
+
+def main():
+    html = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(html.__repr__())
+
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,11 +1,26 @@
 from enum import Enum
 
 class TextType(Enum):
-    TEXT = "text (plain)"
-    BOLD_TEXT = "**Bold text**"
-    ITALIC_TEXT = "_Italic text_"
-    CODE_TEXT = "`Code text`"
-    LINK = "[anchor text](url)"
-    IMAGE = "![alt text](url)"
+    TEXT = "plain"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+        
+    def __eq__(self, other):
+        """
+        Compares self to another TextNode instance (other) and returns True if all of their properties are equal
+        """
+
+        return self == other
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
