703ad6a91769ec9a2be6a9704d58134db0e97e24
Aq1lla <mkorotaj@gmail.com>
2026-04-14 12:58:35 +0000

    6.- Added LeafNode and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestTHTMLNode(unittest.TestCase):
@@ -21,11 +21,28 @@ class TestTHTMLNode(unittest.TestCase):
     def test_is_instance(self):
         node = HTMLNode("p", "This is a text node", [], {"href": "https://www.google.com", "target": "_blank",})
         self.assertIsInstance(node, HTMLNode)
-
+    
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(node.to_html(), "<p>This is a paragraph of text.</p>")
+    
+    def test_leaf_to_html_p_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')    
+        
+    def test_leaf_to_html_p_assertRaises(self):
+        node = LeafNode("a", None, {"href": "https://www.google.com"})
+        self.assertRaises(ValueError)
+    
+    def test_leaf_to_html_notag(self):
+        node = LeafNode(None, "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), "Click me!")  
 
 if __name__ == "__main__":
     unittest.main()
 
 
-
-    
